1d1f27505c28a50b1e99999ff538c22b987e672e
you <youzhenghong1995@outlook.com>
2016-06-03 10:10:15 +0800

    foo

---
+++ app/Nevigate.py
@@ -0,0 +1,26 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/About')
+def AboutPage():
+	return render_template("About.html")
+
+@app.route('/ContactMe')
+def ContactPage():
+	return render_template("ContactMe.html")
+
+
+@app.route('/signin')
+def SignIn():
+	return render_template("SignIn.html")
+
+
+@app.route('/signup')
+def SignUp():
+	return render_template("SignUp.html")
+"""
+@app.route('/signout')
+def SignOut():
+	return render_template("SignOut.html")
+"""
